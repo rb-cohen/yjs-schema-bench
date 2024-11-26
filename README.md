07e68a3d1b06d0311e50d84477dcdbdf4d40182d
Arron Woods <arron.woods@ceros.com>
2024-11-26 10:05:05 +0000

    Initial tests

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
 # yjs-schema-bench
+
+## Running the tests/benchmarks
+
+1. `$ npm install`
+2. `$ npm run test`
+
+## Element schema
+
+This benchmark evaluates the performance of various schemas when handling updates to attributes in a Yjs document. The implementations tested include:
+
+1. **Nested Map**: A nested map of attributes, representing the current setup in the repository. This approach uses a `Y.Map` structure for attributes and deeply nests data within the map.
+
+2. **Key Value Attribute Map**: An implementation using `YKeyValue` from the `y-utilities` package. This schema is optimized for key-value storage but uses a different structure than the nested map.
+
+3. **Array as Attribute Value in Map**: A custom implementation of `YKeyValue`, where a `Y.Array` is stored in the map of each element. This aims to optimize attribute storage by leveraging arrays for specific use cases.
+
+4. **Array as Attribute Value on Root**: Another custom implementation of `YKeyValue`, where a `Y.Array` is stored in the root of the document for each element. This minimizes nesting and centralizes array storage for improved performance in certain operations.
+
+### Results
+
+| Schema                           | Stage   | Doc Size (v1) | Doc Size (v2) | Doc Size (JSON) |
+| -------------------------------- | ------- | ------------- | ------------- | --------------- |
+| Nested Map                       | Insert  | 110.26 KB     | 84.02 KB      | 71.31 KB        |
+|                                  | Updated | 19.17 MB      | 87.74 KB      | 71.10 KB        |
+| Key Value Attribute Map          | Insert  | 114.24 KB     | 110.39 KB     | 107.44 KB       |
+|                                  | Updated | 9.64 MB       | 112.14 KB     | 107.21 KB       |
+| Array as Attribute Value in Map  | Insert  | 106.38 KB     | 92.80 KB      | 75.21 KB        |
+|                                  | Updated | 9.64 MB       | 94.58 KB      | 75.01 KB        |
+| Array as Attribute Value on Root | Insert  | 92.75 KB      | 88.90 KB      | 78.14 KB        |
+|                                  | Updated | 9.62 MB       | 90.69 KB      | 77.95 KB        |
+
+
+### Schema examples
+
+#### Nested Map
+
+```json
+{
+  "elementSchema": {
+    "u205vEIP": {"tagName": "div", "attributes": {"width": "100", "height": "200"}},
+    "VCWK0HH5": {"tagName": "div", "attributes": {"width": "100", "height": "200"}},
+    "MdLTVhKA": {"tagName": "div", "attributes": {"width": "100", "height": "200"}},
+    "WCCadkdT": {"tagName": "div", "attributes": {"width": "100", "height": "200"}},
+    "adSHLH3i": {"tagName": "div", "attributes": {"width": "100", "height": "200"}}
+  }
+}
+```
+
+#### Key Value Attribute Map
+
+```json
+{
+  "elementSchema": {
+    "MvLSWz65": {"tagName": "div"},
+    "AF8ZAL4p": {"tagName": "div"},
+    "KqGrJFC5": {"tagName": "div"},
+    "XKqlanUM": {"tagName": "div"},
+    "UhAAHtVe": {"tagName": "div"}
+  },
+  "MvLSWz65-attributes": [{"key": "width", "val": "100"}, {"key": "height", "val": "200"}],
+  "AF8ZAL4p-attributes": [{"key": "width", "val": "100"}, {"key": "height", "val": "200"}],
+  "KqGrJFC5-attributes": [{"key": "width", "val": "100"}, {"key": "height", "val": "200"}],
+  "XKqlanUM-attributes": [{"key": "width", "val": "100"}, {"key": "height", "val": "200"}],
+  "UhAAHtVe-attributes": [{"key": "width", "val": "100"}, {"key": "height", "val": "200"}]
+}
+```
+
+#### Array as Attribute Value in Map
+
+```json
+{
+  "elementSchema": {
+    "wDN0xu8q": {"tagName": "div", "attributes": [["width", "100"], ["height", "200"]]},
+    "HVlTLjxV": {"tagName": "div", "attributes": [["width", "100"], ["height", "200"]]},
+    "aNmDbwFr": {"tagName": "div", "attributes": [["width", "100"], ["height", "200"]]},
+    "mRHiVCRp": {"tagName": "div", "attributes": [["width", "100"], ["height", "200"]]},
+    "kHt3sRIT": {"tagName": "div", "attributes": [["width", "100"], ["height", "200"]]}
+  }
+}
+```
+
+#### Array as Attribute Value on Root
+
+```json
+{
+  "elementSchema": {
+    "nXOmhIPC": {"tagName": "div"},
+    "pa1DWs8H": {"tagName": "div"},
+    "UWtP6jaq": {"tagName": "div"},
+    "mMN9XHL0": {"tagName": "div"},
+    "FFLpkPxg": {"tagName": "div"}
+  },
+  "nXOmhIPC-attr": [["width", "100"], ["height", "200"]],
+  "pa1DWs8H-attr": [["width", "100"], ["height", "200"]],
+  "UWtP6jaq-attr": [["width", "100"], ["height", "200"]],
+  "mMN9XHL0-attr": [["width", "100"], ["height", "200"]],
+  "FFLpkPxg-attr": [["width", "100"], ["height", "200"]]
+}
+```
---
+++ schema.spec.ts
@@ -0,0 +1,353 @@
+import { describe, it } from 'vitest'
+import { customAlphabet } from 'nanoid'
+import * as Y from 'yjs'
+import { YKeyValue } from 'y-utility/y-keyvalue'
+
+const NUMBER_OF_ELEMENTS = 5 // Set this to 100/1000 to see big differences in doc size
+const NUMBER_OF_UPDATES = 1000
+
+describe('schema benchmark', () => {
+  console.log(
+    `Applying ${NUMBER_OF_UPDATES} updates to ${NUMBER_OF_ELEMENTS} elements`,
+  )
+
+  describe('nested map', () => {
+    it('updates attributes a lot', () => {
+      const doc = new Y.Doc()
+      const elementMap =
+        doc.getMap<Y.Map<string | Y.Map<string>>>('elementSchema')
+
+      const ids = new Set<string>()
+
+      for (let i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+        const id = generateId()
+        const element = new Y.Map<string | Y.Map<string>>()
+        const attributes = new Y.Map<string>()
+        attributes.set('width', '100')
+        attributes.set('height', '200')
+
+        element.set('tagName', 'div')
+        element.set('attributes', attributes)
+
+        elementMap.set(id, element)
+        ids.add(id)
+      }
+
+      const update1v1 = Y.encodeStateAsUpdate(doc)
+      const update1v2 = Y.encodeStateAsUpdateV2(doc)
+      const json1V2 = JSON.stringify(doc.toJSON())
+      console.log(
+        'Insert doc size:',
+        formatBytes(update1v1.byteLength),
+        '(v1)',
+        formatBytes(update1v2.byteLength),
+        '(v2)',
+        formatBytes(json1V2.length),
+        '(json)',
+      )
+
+      // update the width and height of all elements, 1000 times
+      for (let i = 0; i < NUMBER_OF_UPDATES; i++) {
+        ids.forEach((id) => {
+          const elementAttributes = elementMap
+            .get(id)!
+            .get('attributes')! as Y.Map<string>
+          elementAttributes.set('width', getRandomInt(1000).toString())
+          elementAttributes.set('height', getRandomInt(1000).toString())
+        })
+      }
+
+      const update2v1 = Y.encodeStateAsUpdate(doc)
+      const update2v2 = Y.encodeStateAsUpdateV2(doc)
+      const json2V2 = JSON.stringify(doc.toJSON())
+      console.log(
+        'Updated doc size:',
+        formatBytes(update2v1.byteLength),
+        '(v1)',
+        formatBytes(update2v2.byteLength),
+        '(v2)',
+        formatBytes(json2V2.length),
+        '(json)',
+      )
+    })
+  })
+
+  describe('key value attribute map', () => {
+    it('updates attributes a lot', () => {
+      const doc = new Y.Doc()
+      const elementMap = doc.getMap<Y.Map<string>>('elementSchema')
+
+      const ids = new Set<string>()
+      const attributeKeyValueMap = new Map<string, YKeyValue<string>>()
+
+      for (let i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+        const id = generateId()
+        const element = new Y.Map<string>()
+
+        element.set('tagName', 'div')
+        elementMap.set(id, element)
+
+        const attributesArray = doc.getArray<{ key: string; val: string }>(
+          `${id}-attributes`,
+        )
+        const attributes = new YKeyValue<string>(attributesArray)
+        attributes.set('width', '100')
+        attributes.set('height', '200')
+
+        ids.add(id)
+        attributeKeyValueMap.set(id, attributes)
+      }
+
+      const update1v1 = Y.encodeStateAsUpdate(doc)
+      const update1v2 = Y.encodeStateAsUpdateV2(doc)
+      const json1V2 = JSON.stringify(doc.toJSON())
+      console.log(
+        'Insert doc size:',
+        formatBytes(update1v1.byteLength),
+        '(v1)',
+        formatBytes(update1v2.byteLength),
+        '(v2)',
+        formatBytes(json1V2.length),
+      )
+
+      // update the width and height of all elements, X times
+      for (let i = 0; i < NUMBER_OF_UPDATES; i++) {
+        ids.forEach((id) => {
+          const elementAttributes = attributeKeyValueMap.get(id)!
+          elementAttributes.set('width', getRandomInt(1000).toString())
+          elementAttributes.set('height', getRandomInt(1000).toString())
+        })
+      }
+
+      const update2v1 = Y.encodeStateAsUpdate(doc)
+      const update2v2 = Y.encodeStateAsUpdateV2(doc)
+      const json2V2 = JSON.stringify(doc.toJSON())
+      console.log(
+        'Updated doc size:',
+        formatBytes(update2v1.byteLength),
+        '(v1)',
+        formatBytes(update2v2.byteLength),
+        '(v2)',
+        formatBytes(json2V2.length),
+        '(json)',
+      )
+    })
+  })
+
+  describe('array as attribute value in map', () => {
+    it('updates attributes a lot', () => {
+      const doc = new Y.Doc()
+      const elementMap =
+        doc.getMap<Y.Map<string | Y.Array<string[]>>>('elementSchema')
+
+      const ids = new Set<string>()
+
+      for (let i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+        const id = generateId()
+        const element = new Y.Map<string | Y.Array<string[]>>()
+
+        const attributes = new Y.Array<string[]>()
+        attributes.push([
+          ['width', '100'],
+          ['height', '200'],
+        ])
+
+        element.set('tagName', 'div')
+        element.set('attributes', attributes)
+
+        elementMap.set(id, element)
+        ids.add(id)
+      }
+
+      const update1v1 = Y.encodeStateAsUpdate(doc)
+      const update1v2 = Y.encodeStateAsUpdateV2(doc)
+      const json1V2 = JSON.stringify(doc.toJSON())
+      console.log(
+        'Insert doc size:',
+        formatBytes(update1v1.byteLength),
+        '(v1)',
+        formatBytes(update1v2.byteLength),
+        '(v2)',
+        formatBytes(json1V2.length),
+        '(json)',
+      )
+
+      // update the width and height of all elements, 1000 times
+      for (let i = 0; i < NUMBER_OF_UPDATES; i++) {
+        ids.forEach((id) => {
+          const elementAttributes = elementMap
+            .get(id)!
+            .get('attributes')! as Y.Array<string[]>
+
+          const lengthToDate = elementAttributes.length
+
+          const indexesToRemove: number[] = []
+          for (let j = 0; j < lengthToDate; j++) {
+            const attribute = elementAttributes.get(j)
+            if (attribute[0] === 'width' || attribute[0] === 'height') {
+              indexesToRemove.push(j)
+            }
+          }
+
+          const groups = groupConsecutiveIndexes(indexesToRemove)
+          groups.forEach(([start, length]) => {
+            elementAttributes.delete(start, length)
+          })
+
+          elementAttributes.push([
+            ['width', getRandomInt(1000).toString()],
+            ['height', getRandomInt(1000).toString()],
+          ])
+        })
+      }
+
+      const update2v1 = Y.encodeStateAsUpdate(doc)
+      const update2v2 = Y.encodeStateAsUpdateV2(doc)
+      const json2V2 = JSON.stringify(doc.toJSON())
+      console.log(
+        'Updated doc size:',
+        formatBytes(update2v1.byteLength),
+        '(v1)',
+        formatBytes(update2v2.byteLength),
+        '(v2)',
+        formatBytes(json2V2.length),
+        '(json)',
+      )
+    })
+  })
+
+  describe('array as attribute value on root', () => {
+    it('updates attributes a lot', () => {
+      const doc = new Y.Doc()
+      const elementMap = doc.getMap<Y.Map<string>>('elementSchema')
+
+      const ids = new Set<string>()
+
+      for (let i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+        const id = generateId()
+        const element = new Y.Map<string>()
+
+        const attributes = doc.getArray<string[]>(`${id}-attr`)
+        attributes.push([
+          ['width', '100'],
+          ['height', '200'],
+        ])
+
+        element.set('tagName', 'div')
+
+        elementMap.set(id, element)
+        ids.add(id)
+      }
+
+      const update1v1 = Y.encodeStateAsUpdate(doc)
+      const update1v2 = Y.encodeStateAsUpdateV2(doc)
+      const json1V2 = JSON.stringify(doc.toJSON())
+      console.log(
+        'Insert doc size:',
+        formatBytes(update1v1.byteLength),
+        '(v1)',
+        formatBytes(update1v2.byteLength),
+        '(v2)',
+        formatBytes(json1V2.length),
+        '(json)',
+      )
+
+      // update the width and height of all elements, 1000 times
+      for (let i = 0; i < NUMBER_OF_UPDATES; i++) {
+        ids.forEach((id) => {
+          const elementAttributes = doc.getArray<string[]>(`${id}-attr`)
+          const lengthToDate = elementAttributes.length
+
+          const indexesToRemove: number[] = []
+          for (let j = 0; j < lengthToDate; j++) {
+            const attribute = elementAttributes.get(j)
+            if (attribute[0] === 'width' || attribute[0] === 'height') {
+              indexesToRemove.push(j)
+            }
+          }
+
+          const groups = groupConsecutiveIndexes(indexesToRemove)
+          groups.forEach(([start, length]) => {
+            elementAttributes.delete(start, length)
+          })
+
+          elementAttributes.push([
+            ['width', getRandomInt(1000).toString()],
+            ['height', getRandomInt(1000).toString()],
+          ])
+        })
+      }
+
+      const update2v1 = Y.encodeStateAsUpdate(doc)
+      const update2v2 = Y.encodeStateAsUpdateV2(doc)
+      const json2V2 = JSON.stringify(doc.toJSON())
+      console.log(
+        'Updated doc size:',
+        formatBytes(update2v1.byteLength),
+        '(v1)',
+        formatBytes(update2v2.byteLength),
+        '(v2)',
+        formatBytes(json2V2.length),
+        '(json)',
+      )
+    })
+  })
+})
+
+// assumes indexes is sorted already
+function groupConsecutiveIndexes(indexes: number[]): [number, number][] {
+  if (indexes.length === 0) {
+    return []
+  }
+
+  const result: [number, number][] = []
+  let start = indexes[0]
+  let length = 1
+
+  for (let i = 1; i < indexes.length; i++) {
+    if (indexes[i] === indexes[i - 1] + 1) {
+      // Consecutive index
+      length++
+    } else {
+      // Add the previous group to the result
+      result.push([start, length])
+
+      // Start a new group
+      start = indexes[i]
+      length = 1
+    }
+  }
+
+  // Add the last group to the result
+  result.push([start, length])
+
+  return result
+}
+
+function formatBytes(bytes: number): string {
+  if (bytes === 0) return '0 Bytes'
+
+  const units = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  const index = Math.floor(Math.log(bytes) / Math.log(1024))
+  const value = bytes / Math.pow(1024, index)
+
+  return `${value.toFixed(2)} ${units[index]}`
+}
+
+function getRandomInt(max: number): number {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+export const alphaNumericId = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  8,
+)
+
+export const alphaId = customAlphabet(
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  8,
+)
+
+export function generateId() {
+  return `${alphaId(1)}${alphaNumericId(7)}`
+}
